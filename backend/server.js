f664baf525348aa8429de5a2a802b56f70db6c08
Sudip Khatri <sudipkhatri@192-168-1-100.tpgi.com.au>
2022-10-11 11:35:03 +1100

    promise changed for logout

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if(process.env.NODE_ENV === "production"){
     const __dirname = path.dirname(__filename);
     app.use(express.static(path.join("frontend/build")));
     app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     });  
 }
 
